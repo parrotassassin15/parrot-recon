804d6459146fb2269b1e5691244d58197798e0fc
parrotassassin15 <54908247+parrotassassin15@users.noreply.github.com>
2022-01-10 18:32:47 -0500

    Add files via upload

--- webdav/webdav.py
+++ webdav/webdav.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python3
 
+# beta webdav handler
+# dev: parrotassassin15
+
+
+# imports
+from time import sleep as delay 
+from os import system as s
+
+
+global host
+host = "http://127.0.0.1:8098"
+
+def main():
+    s("sudo service wsgidav start")
+    print("Please wait...")
+    for x in reversed(range(1,4)):
+        print(x)
+        delay(2)
+    print("web dav server started. visit " + host)
+
+main()
